549e3149e4037004a36c6c518d242ac49533961b
Admas Molla <molla.admas@gmail.com>
2022-02-04 18:50:52 -0800

    adding a security test screen

---
+++ lib/pages/secure_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:key_enclave/key_enclave.dart';
+
+class SecureScreen extends StatefulWidget {
+  SecureScreen();
+
+  @override
+  SecureScreenState createState() {
+    return new SecureScreenState();
+  }
+}
+
+class SecureScreenState extends State<SecureScreen> {
+  String publicKey = '';
+  String signMessage = "";
+  String TAG = "KEY_TAG";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () async {
+                final pub = await KeyEnclave().generateKeyPair(TAG);
+                setState(() {
+                  this.publicKey = pub;
+                });
+              },
+              child: Text("GENERATE KEY PAIR"),
+            ),
+            Text("PUBLIC KEY " + this.publicKey),
+            RaisedButton(
+              onPressed: () async {
+                try {
+                  // change message that you want to sign
+                  final signed =
+                      await KeyEnclave().signMessage(TAG, "SECURE MESSAGE");
+                  setState(() {
+                    this.signMessage = signed;
+                  });
+                } catch (e) {
+                  setState(() {
+                    this.signMessage = e.toString();
+                  });
+                }
+              },
+              child: Text("SIGN MESSAGE"),
+            ),
+            Text("SIGN MESSAGE " + this.signMessage)
+          ],
+        ),
+      ),
+    );
+  }
+}
